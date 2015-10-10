7fa745975561eb41a3affcc918783b98ff47d41f
Barnabas Bulpett <bbulpett@charter.net>
2015-10-10 16:11:25 -0400

    Fix circular dependency in messages_channel.rb

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,4 +1,4 @@
-class MessagesChannel < ApplicationCable::MessagesChannel
+class MessagesChannel < ApplicationCable::Channel
 	# subscribed method streams client's broadcasts to the messages stream (when they're subscribed).
 	def subscribed
 		stream_from 'messages'
