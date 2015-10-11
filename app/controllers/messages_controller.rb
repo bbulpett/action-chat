e7bcdf48ab3f6df1daf5e4756fc1e093607dfeb3
Barnabas Bulpett <bbulpett@charter.net>
2015-10-11 18:42:59 -0400

    Current username method, Fix views to render messages properly

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,19 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
   end
   def create
+    message = Message.new(person_params)
+    message.username = current_username
   	# broadcast incoming messages to the messages stream
   	ActionCable.server.broadcast 'messages',
-	  	message: params[:message][:body],
-	  	username: cookies.signed[:username]
-  # respond with status code for every successful request
+	  	message: MessagesController.render(message)
+    # respond with status code for every successful request
   	head :ok
   end
+
+  private
+    def person_params
+      params.require(:message).permit(:body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	resources :messages, only: [:index, :create]
-	resources :sessions, only: [:new, :create]
+	resources :sessions, only: :create
 
 	root 'sessions#new'
 end
